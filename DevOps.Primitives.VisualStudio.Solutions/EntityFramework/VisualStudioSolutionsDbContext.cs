1f4e9da3fc6708075ba0820b17a6a0090dc2ada0
Christopher Dorst <chris@dorst.email>
2017-12-08 10:38:48 -0600

    Adding unique indexes

--- DevOps.Primitives.VisualStudio.Solutions/EntityFramework/VisualStudioSolutionsDbContext.cs
+++ DevOps.Primitives.VisualStudio.Solutions/EntityFramework/VisualStudioSolutionsDbContext.cs
@@ -12,5 +12,31 @@ namespace DevOps.Primitives.VisualStudio.Solutions.EntityFramework
         public DbSet<Solution> Solutions { get; set; }
         public DbSet<SolutionFolder> SolutionFolders { get; set; }
         public DbSet<SolutionProject> SolutionProjects { get; set; }
+        public DbSet<SolutionProjectList> SolutionProjectLists { get; set; }
+        public DbSet<SolutionProjectListAssociation> SolutionProjectListAssociations { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            AddIndexes(modelBuilder);
+        }
+
+        private void AddIndexes(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Solution>()
+                .HasIndex(e => new { e.Guid, e.NameId, e.SolutionFolderListId }).IsUnique();
+            modelBuilder.Entity<SolutionFolder>()
+                .HasIndex(e => new { e.Guid, e.NameId, e.SolutionProjectListId }).IsUnique();
+            modelBuilder.Entity<SolutionFolderList>()
+                .HasIndex(e => new { e.ListIdentifierId }).IsUnique();
+            modelBuilder.Entity<SolutionFolderListAssociation>()
+                .HasIndex(e => new { e.SolutionFolderId, e.SolutionFolderListId }).IsUnique();
+            modelBuilder.Entity<SolutionProject>()
+                .HasIndex(e => new { e.Guid, e.NameId, e.PathRelativeToSolutionId }).IsUnique();
+            modelBuilder.Entity<SolutionProjectList>()
+                .HasIndex(e => new { e.ListIdentifierId }).IsUnique();
+            modelBuilder.Entity<SolutionProjectListAssociation>()
+                .HasIndex(e => new { e.SolutionProjectId, e.SolutionProjectListId }).IsUnique();
+        }
     }
 }
