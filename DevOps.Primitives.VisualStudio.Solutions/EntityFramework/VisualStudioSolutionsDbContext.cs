30ec888508aa02fca7c0016e5a4b49d4a0ffc2d6
Christopher Dorst <chris@dorst.email>
2017-12-08 10:10:23 -0600

    Adding VisualStudioSolutionsDbContext

---
+++ DevOps.Primitives.VisualStudio.Solutions/EntityFramework/VisualStudioSolutionsDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DevOps.Primitives.Strings.EntityFramework;
+using Microsoft.EntityFrameworkCore;
+
+namespace DevOps.Primitives.VisualStudio.Solutions.EntityFramework
+{
+    public class VisualStudioSolutionsDbContext : UniqueStringsDbContext
+    {
+        public VisualStudioSolutionsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Solution> Solutions { get; set; }
+        public DbSet<SolutionFolder> SolutionFolders { get; set; }
+        public DbSet<SolutionProject> SolutionProjects { get; set; }
+    }
+}
